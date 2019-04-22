6426a716a93e123dca99fa6af58d91dc78b27c40
Van Dat <fanh.troll@gmail.com>
2019-04-22 16:23:16 +0700

    finish data user-finish layout user-show user success

--- app/src/main/java/huedev/org/ui/activity/user/UserContact.java
+++ app/src/main/java/huedev/org/ui/activity/user/UserContact.java
@@ -1,4 +1,17 @@
 package huedev.org.ui.activity.user;
 
+import java.util.List;
+
+import huedev.org.data.model.User;
+import huedev.org.ui.base.activity.BasePresenter;
+import huedev.org.ui.base.activity.BaseView;
+
 public interface UserContact {
+    interface View extends BaseView {
+        void usersList(List<User> userList);
+    }
+
+    interface Presenter extends BasePresenter<View> {
+        void users();
+    }
 }
